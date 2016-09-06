7fa416a5dd0cbcc4d79a911722502998f60a4d25
The-Chanman <ecphylo@gmail.com>
2016-09-06 13:16:36 -0700

    should hookup now

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ var graphs = [
 //Get all events
 
 particle.getEventStream({ auth: '9e61d1cf7b89fb5f3e54c6f76a2bdac46cf569eb' }).then(function(stream) {
-  stream.on('event', function(data) {
+  stream.on('car_crash', function(data) {
     debugger
     console.log("On Event: " + set + data);
     openThatDamnedModal();
